d9f17d5febf55908e51673bac8203623601cefd8
bektur99 <bektur040504@gmail.com>
2022-10-27 01:35:17 +0300

    bug

--- app/src/main/java/com/example/hw5/MainActivity.java
+++ app/src/main/java/com/example/hw5/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.dz_5_ms_2;
+package com.example.hw5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.dz_5_ms_2.R;
 
 public class MainActivity extends AppCompatActivity {
 
